45199b53dd629083862dd039d086457d565cc6ac
yinxunyang <yinxunyang>
2019-12-04 11:36:10 +0800

    ass

--- src/main/java/com/bestvike/standplat/dao/SysDictDao.java
+++
@@ -1,9 +0,0 @@
-package com.bestvike.standplat.dao;
-
-import com.bestvike.standplat.data.SysDict;
-import org.springframework.stereotype.Repository;
-import tk.mybatis.mapper.common.Mapper;
-
-@Repository
-public interface SysDictDao extends Mapper<SysDict> {
-}
